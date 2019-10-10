cddc50904f1653edd29b8e11d8bfb9fafbc029d4
AMNeverova <anastacia1302@yandex.ru>
2019-10-10 10:10:01 +0300

    no globals, no cycle

--- task3.js
+++ task3.js
@@ -1,25 +1,17 @@
 const checkPossibleEquality = (string1, string2) => {
     let array1 = string1.toLowerCase().split('');
     let array2 = string2.toLowerCase().split('');
-    let isPossible = true;
     if (array1.length !== array2.length) {
         return false;
     }
-    array1Even = array1.filter((letter, index, array) => index % 2 == 0).sort();
-    array1Odd = array1.filter((letter, index, array) => index % 2 == 1).sort();
-    array1Full = array1Even.concat(array1Odd)
-    array2Even = array2.filter((letter, index, array) => index % 2 == 0).sort();
-    array2Odd = array2.filter((letter, index, array) => index % 2 == 1).sort();
-    array2Full = array2Even.concat(array2Odd);
+    const array1Even = array1.filter((letter, index, array) => index % 2 == 0).sort();
+    const array1Odd = array1.filter((letter, index, array) => index % 2 == 1).sort();
+    const array1Full = array1Even.concat(array1Odd)
+    const array2Even = array2.filter((letter, index, array) => index % 2 == 0).sort();
+    const array2Odd = array2.filter((letter, index, array) => index % 2 == 1).sort();
+    const array2Full = array2Even.concat(array2Odd);
 
-    for (let i = 0; i < array1Full.length; i++) {
-        if (array1Full[i] == array2Full[i]) {
-            continue
-        } else {
-            return false
-        }
-    }
-    return isPossible;
+    return array1Full.join('') === array2Full.join('');
 }
 
-console.log(checkPossibleEquality('qWerty', 'ytqwer'));
+console.log(checkPossibleEquality('abcabcabc', 'bcababcac'));
